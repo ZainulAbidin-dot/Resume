8a256601c260a2d1aeffa95ed6ce74021ca1c346
Zain Ul Abidin <zchxvr@gmail.com>
2024-10-17 23:40:57 +0300

    Remove Computer 3D model

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const ServiceCard = ({ index, title, icon }) => {
 
 const About = () => {
   return (
-    <>
+    <div className='mt-[-80px]'>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
@@ -40,7 +40,7 @@ const About = () => {
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
