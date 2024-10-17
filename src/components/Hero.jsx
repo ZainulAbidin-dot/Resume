91810fdbe54cd8824d26ab1bdaf030156f73944a
Zain Ul Abidin <zchxvr@gmail.com>
2024-10-17 23:03:11 +0300

    Remove Computer 3D model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,9 +19,9 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center '>
+      {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center '>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div animate={{
@@ -33,7 +33,7 @@ const Hero = () => {
             }} className="w-3 h-3 rounded-full bg-secondary mb-1" />
           </div>
         </a>
-      </div>
+      </div> */}
     </section>
   )
 }
